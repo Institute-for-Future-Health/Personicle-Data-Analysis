d67982f7f0795045a41dc46643de8cab2c545553
hyungiko <hyungiko@uci.edu>
2018-12-21 09:10:27 -0800

    Add files via upload

--- Unsupervised/main.py
+++ Unsupervised/main.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     k = 4
     algorithm = 'spectral'
     # algorithm = 'girvan_newman'
-    feature_set = ['peak', 'time_to_get_max', 'mean', 'variation']
+    feature_set = ['peak_avg', 'mean', 'peak']
     result_file = 'data_4_sp_1213'
     cluster = 3
-    #
+
     unsupervised = unsupervised(k, algorithm, feature_set)
-    unsupervised.set_data_frame(result_file, cluster)
+    # unsupervised.set_data_frame(result_file, cluster)
     unsupervised.run()
+
+    # unsupervised.draw_mod_plot_for_spectral_clustering(30)
 
     # dataFrame = unsupervised.get_selected_subset_data()
     # length = len(dataFrame.keys())
@@ -23,3 +25,6 @@ if __name__ == "__main__":
 
     # makeGraph('kneighbors', numpy_array)
     # makeGraph('radius_neighbors', numpy_array)
+
+    # k = 1 - -2.5383659858371268e-18
+    # k = 2 - 0.44265528873494325
--- Unsupervised/unsupervised.py
+++ Unsupervised/unsupervised.py
@@ -4,8 +4,16 @@ import operator
 
 import numpy as np
 import networkx as nx
-from networkx.algorithms.community import girvan_newman
+from matplotlib.pyplot import plot
+from networkx.algorithms.community import girvan_newman, greedy_modularity_communities, is_partition
+from networkx.algorithms.community.quality import NotAPartition
+from sklearn.manifold.spectral_embedding_ import _set_diag
+from scipy.sparse.csgraph import laplacian as csgraph_laplacian
+
+from sklearn.neighbors import kneighbors_graph
 from sklearn.utils import shuffle
+from sklearn.utils.arpack import eigsh
+from sklearn.utils.graph import graph_laplacian
 
 from Experiment.featureSelection import feature_selection
 from Experiment.globalVariable import global_variable
@@ -110,6 +118,8 @@ class unsupervised:
                 tick_label.set_color('y')
             elif selected_label == 3:
                 tick_label.set_color('r')
+            elif selected_label == 4:
+                tick_label.set_color('m')
             else:
                 tick_label.set_color('c')
 
@@ -123,10 +133,45 @@ class unsupervised:
 
         return clustering
 
+    def draw_mod_plot_for_spectral_clustering(self, max_k: int):
+        # it is normalized Q
+        # it needs to update graph - sample2.txt
+        numpy_array = self._dataFrame.values
+        mod_result = []
+        x = []
+
+        for k in range(1, max_k+1):
+            clustering = SpectralClustering(n_clusters=k, affinity='nearest_neighbors', n_init=15).fit_predict(
+                numpy_array)
+
+            result = []
+            for count in range(0, k):
+                result.append([])
+
+            for count in range(0, len(clustering)):
+                result[clustering[count]].append(str(count))
+
+            G = nx.read_edgelist(global_variable.graph_path)
+            mod = nx.algorithms.community.modularity(G, result)
+            mod_result.append(mod)
+            x.append(k)
+
+            print('k =', k, ', modularity = ', mod)
+
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(x, mod_result, '.')
+        plt.xlabel("Number of cluster")
+        plt.ylabel("Normalized Q")
+        plt.show()
+
     def _girvan_newman(self) -> list:
         k = self._k - 1
-
         G = nx.read_edgelist(global_variable.graph_path)
+
+        # self.modularity(G, G.nodes)
+        # nx.algorithms.community.modularity(G, [{0, 1, 2}, {3, 4, 5}])
+
         comp = girvan_newman(G)
         result = ()
         for communities in itertools.islice(comp, k):
@@ -221,3 +266,70 @@ class unsupervised:
                     attr_list.append('')
 
             csvWrite.writerow(attr_list)
+
+    def predict_k(self, affinity_matrix):
+        """
+        Predict number of clusters based on the eigengap.
+        Parameters
+        ----------
+        affinity_matrix : array-like or sparse matrix, shape: (n_samples, n_samples)
+            adjacency matrix.
+            Each element of this matrix contains a measure of similarity between two of the data points.
+        Returns
+        ----------
+        k : integer
+            estimated number of cluster.
+        Note
+        ---------
+        If graph is not fully connected, zero component as single cluster.
+        References
+        ----------
+        A Tutorial on Spectral Clustering, 2007
+            Luxburg, Ulrike
+            http://www.kyb.mpg.de/fileadmin/user_upload/files/publications/attachments/Luxburg07_tutorial_4488%5b0%5d.pdf
+        """
+
+        """
+        If normed=True, L = D^(-1/2) * (D - A) * D^(-1/2) else L = D - A.
+        normed=True is recommended.
+        """
+        # normed_laplacian, dd = graph_laplacian(affinity_matrix, normed=True, return_diag=True)
+        normed_laplacian, dd = csgraph_laplacian(affinity_matrix, normed=True,
+                                                 return_diag=True)
+
+        laplacian = _set_diag(normed_laplacian, 1, True)
+
+        """
+        n_components size is N - 1.
+        Setting N - 1 may lead to slow execution time...
+        """
+        n_components = affinity_matrix.shape[0] - 1
+
+        """
+        shift-invert mode
+        The shift-invert mode provides more than just a fast way to obtain a few small eigenvalues.
+        http://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html
+        The normalized Laplacian has eigenvalues between 0 and 2.
+        I - L has eigenvalues between -1 and 1.
+        """
+        eigenvalues, eigenvectors = eigsh(-laplacian, k=n_components, which="LM", sigma=1.0, maxiter=5000)
+        eigenvalues = -eigenvalues[::-1]  # Reverse and sign inversion.
+
+        max_gap = 0
+        gap_pre_index = 0
+        test = []
+        x_axis = []
+        for i in range(1, eigenvalues.size):
+            # if len(x_axis) <= 20:
+            x_axis.append(i)
+            test.append(eigenvalues[i])
+            gap = eigenvalues[i] - eigenvalues[i - 1]
+            if gap > max_gap:
+                max_gap = gap
+                gap_pre_index = i - 1
+
+        k = gap_pre_index + 1
+
+        plot(x_axis, test, '*')
+
+        return k
